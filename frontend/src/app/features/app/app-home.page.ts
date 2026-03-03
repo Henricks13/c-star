31205d7e926c58ceb8ffa7692e7db2a18e5946e3
this.Henricks <128629444+Henricks13@users.noreply.github.com>
2026-03-02 23:17:31 -0300

    feat(frontend): integrate berry free angular admin template

--- frontend/src/app/features/app/app-home.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-student-home',
-  templateUrl: './app-home.page.html',
-  styleUrl: './app-home.page.scss'
-})
-export class AppHomePage {}
