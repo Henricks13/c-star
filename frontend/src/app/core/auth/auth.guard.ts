22ca2a2bdd3d994de685240a2606feaeaf57bc4f
this.Henricks <128629444+Henricks13@users.noreply.github.com>
2026-03-23 19:20:40 -0300

    fix: redirecionar nao autenticado para login e ocultar google no login

--- frontend/src/app/core/auth/auth.guard.ts
+++ frontend/src/app/core/auth/auth.guard.ts
@@ -14,6 +14,6 @@ export class AuthGuard implements CanActivate {
       return true;
     }
 
-    return this.router.createUrlTree(['/access-denied']);
+    return this.router.createUrlTree(['/login']);
   }
 }
